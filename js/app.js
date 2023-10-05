3223a698b318e0fe89e443f4ea6b79dbca48766f
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-05 16:40:53 +0200

    ADD function for calc total price

--- js/app.js
+++ js/app.js
@@ -19,28 +19,50 @@ const DISCOUNT_UNDER_18 = 0.2;
 const DISCOUNT_OVER_65 = 0.4;
 const DISCOUNT_NOT = 0;
 //     - Creazione del evento onclick
-//         - Recupero il nome dal input
-//             - recupero il valore presente nell'input e lo salvo in una variabile passengerName
-//         - Recupero il Surname dal input
-//             - recupero il valore presente nell'input e lo salvo in una variabile passengerSurname
-//         - Recupero il distanza dal input
-//             - recupero il valore presente nell'input e lo salvo in una variabile distanceTrip
-//         - Recupero la tipologia dello sconto input
-//             - recupero il valore presente nell'input e lo salvo in una variabile
-//         - Creare la variabile "priceBasic"
-//         - Calcolare il prezzo base 
-//              priceBasic = distanceTrip * PRICE_KM
-//         - Creare la variabile "passengerDiscountEntity"
-//             -  SE (inputDiscountSelectDOMElement.value == 0) 
-//                 - passengerDiscountEntity = DISCOUNT_UNDER_18
-//             ALTRIMENTI SE (inputDiscountSelectDOMElement.value == 1) 
-//                 - passengerDiscountEntity = DISCOUNT_OVER_65
-//             ALTRIMENTI () 
-//                 - passengerDiscountEntity = DISCOUNT_NOT
-//         - Creare la variabile "dicountPassenger"
-//             - discountPassenger = priceBasic * passengerDiscountEntity
-//         - Creare la variabile "totalPrice"
-//             - totalPrice = priceBasic - discountPassenger 
+btnDOMElement.addEventListener("click", function() {
+    //         - Recupero il nome dal input
+    //             - recupero il valore presente nell'input e lo salvo in una variabile passengerName
+    const passengerName = inputNameDOMElement.value.trim();
+    console.log("Nome del passegero:" + " " + passengerName, typeof passengerName);
+    //         - Recupero il Surname dal input
+    //             - recupero il valore presente nell'input e lo salvo in una variabile passengerSurname
+    const passengerSurname = inputSurnameDOMElement.value.trim();
+    console.log("Cognome del passegero:" + " " + passengerSurname, typeof passengerSurname);
+    //         - Recupero il distanza dal input
+    //             - recupero il valore presente nell'input e lo salvo in una variabile distanceTrip
+    const distanceTrip = Number(parseFloat(inputDistanceTripDOMElement.value.replace(/,/g, ".").replace(/\s/g, '')).toFixed(2))
+    console.log("Distanza del viaggio:" + " " + distanceTrip, typeof distanceTrip);
+    //         - Recupero la tipologia dello sconto input
+    //             - recupero il valore presente nell'input e lo salvo in una variabile "passengerDiscountEntity"
+    //             -  SE (inputDiscountSelectDOMElement.value == 0) 
+    //                 - passengerDiscountEntity = DISCOUNT_UNDER_18
+    //             ALTRIMENTI SE (inputDiscountSelectDOMElement.value == 1) 
+    //                 - passengerDiscountEntity = DISCOUNT_OVER_65
+    //             ALTRIMENTI () 
+    //                 - passengerDiscountEntity = DISCOUNT_NOT
+    if (inputDiscountSelectDOMElement.value == 0) {
+        var passengerDiscountEntity = DISCOUNT_UNDER_18
+        console.log("Percentuale di sconto è " + DISCOUNT_UNDER_18 * 100 + "%", typeof passengerDiscountEntity);
+    } else if (inputDiscountSelectDOMElement.value == 2) {
+        var passengerDiscountEntity = DISCOUNT_OVER_65
+        console.log("Percentuale di sconto è " + DISCOUNT_OVER_65 * 100 + "%", typeof passengerDiscountEntity);
+    } else {
+        var passengerDiscountEntity = DISCOUNT_NOT
+        console.log("Percentuale di sconto è " + DISCOUNT_NOT * 100 + "%", typeof passengerDiscountEntity);
+    }
+    //         - Creare la variabile "priceBasic" e calcolare il suo valore
+    const priceBasic = Number((distanceTrip * PRICE_KM).toFixed(2));
+    console.log("Prezzo di base è " + priceBasic + " " + "Euro", typeof priceBasic);
+    //         - Creare la variabile "dicountPassenger"
+    //             - discountPassenger = priceBasic * passengerDiscountEntity
+    const discountPassenger = priceBasic * passengerDiscountEntity;
+    console.log("Sconto in EURO è " + discountPassenger + " " + "Euro", typeof discountPassenger);
+    //         - Creare la variabile "totalPrice"
+    //             - totalPrice = priceBasic - discountPassenger 
+    const totalPrice = Number((priceBasic - discountPassenger).toFixed(2));
+    console.log("Prezzo totale è " + totalPrice + " " + "Euro", typeof totalPrice);
+});
+      
 //         - Creare console log per controllo
 // # Creazione biglietto
 
