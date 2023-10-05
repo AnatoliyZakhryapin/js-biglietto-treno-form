25c86d24d1f030c399a12d0349d91c643ce260c6
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-05 15:59:14 +0200

    ADD Dom Element : create cons Dom Element for with input

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// # Calcolo del prezzo del biglietto del treno
+
+// - Creare DOM element per ogni tipo di input
+
+const inputNameDOMElement = document.getElementById("passengerName");
+const inputSurnameDOMElement = document.getElementById("passengerSurname");
+const inputDistanceTripDOMElement = document.getElementById("distanceTrip");
+const inputDiscountSelectDOMElement = document.getElementById("discount");
+const btnDOMElement = document.getElementById('btn')
+
+
+
+
+
+
+
+
+
+
