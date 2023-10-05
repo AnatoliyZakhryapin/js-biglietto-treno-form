a9f8e24b735c3b22c214d8c565fee81673fa81b0
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-05 16:02:07 +0200

    ADD const date fix

--- js/app.js
+++ js/app.js
@@ -8,7 +8,43 @@ const inputDistanceTripDOMElement = document.getElementById("distanceTrip");
 const inputDiscountSelectDOMElement = document.getElementById("discount");
 const btnDOMElement = document.getElementById('btn')
 
+// - Recupero dei dai tramite il button e il calcolo del prezzo del viaggio
+//     - Creazione delle variabile con i dati fissi
+//          - Creare la variabile "PRICE_KM" con valore 0.21 (valore euro per km)
+//          - Creare la variabile "DISCOUNT_UNDER_18" 20% in decimali
+//          - Creare la variabile "DISCOUNT_OVER_65" 40% in diecimali
+//          - Creare la variabile "DISCOUNT_NOT" 0% in decimal
+const PRICE_KM = 0.21;
+const DISCOUNT_UNDER_18 = 0.2;
+const DISCOUNT_OVER_65 = 0.4;
+const DISCOUNT_NOT = 0;
+//     - Creazione del evento onclick
+//         - Recupero il nome dal input
+//             - recupero il valore presente nell'input e lo salvo in una variabile passengerName
+//         - Recupero il Surname dal input
+//             - recupero il valore presente nell'input e lo salvo in una variabile passengerSurname
+//         - Recupero il distanza dal input
+//             - recupero il valore presente nell'input e lo salvo in una variabile distanceTrip
+//         - Recupero la tipologia dello sconto input
+//             - recupero il valore presente nell'input e lo salvo in una variabile
+//         - Creare la variabile "priceBasic"
+//         - Calcolare il prezzo base 
+//              priceBasic = distanceTrip * PRICE_KM
+//         - Creare la variabile "passengerDiscountEntity"
+//             -  SE (inputDiscountSelectDOMElement.value == 0) 
+//                 - passengerDiscountEntity = DISCOUNT_UNDER_18
+//             ALTRIMENTI SE (inputDiscountSelectDOMElement.value == 1) 
+//                 - passengerDiscountEntity = DISCOUNT_OVER_65
+//             ALTRIMENTI () 
+//                 - passengerDiscountEntity = DISCOUNT_NOT
+//         - Creare la variabile "dicountPassenger"
+//             - discountPassenger = priceBasic * passengerDiscountEntity
+//         - Creare la variabile "totalPrice"
+//             - totalPrice = priceBasic - discountPassenger 
+//         - Creare console log per controllo
+// # Creazione biglietto
 
+// - Pescare Dom Elementi con id e assegnare i valori di interesse
 
 
 
